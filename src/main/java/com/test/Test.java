b8e14e680d44031c55fba4f308c6affdbcd42df7
Alex <askochin@gmail.com>
2022-07-12 23:13:45 +0300

    base files

---
+++ src/main/java/com/test/Test.java
@@ -0,0 +1,8 @@
+package com.test;
+
+public class Test {
+
+    public void test() {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/test/java/com/test/TestTest.java
@@ -0,0 +1,13 @@
+package com.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestTest {
+
+    @Test
+    void test() {
+        new com.test.Test().test();
+    }
+}
